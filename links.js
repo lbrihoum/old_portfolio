debe12ef5618b252725ea4eebfd63beae218631a
Lina Yara Brihoum <brihoumlina@gmail.com>
2020-02-16 20:12:26 -0500

    I'm trying

---
+++ links.js
@@ -0,0 +1,5 @@
+$('a.yourlink').click(function(e) {
+    e.preventDefault();
+    window.open('https://github.com/lbrihoum/FluffyFeelings');
+    window.open('https://github.com/lbrihoum/Doggos');
+});
