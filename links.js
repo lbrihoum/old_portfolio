7b7df8c7a8a65ceaa06752d78e1728d0996bf2f1
Lina Yara Brihoum <brihoumlina@gmail.com>
2020-07-20 23:41:40 -0400

    Fixing up Resume

--- links.js
+++
@@ -1,5 +0,0 @@
-$('a.yourlink').click(function(e) {
-    e.preventDefault();
-    window.open('https://github.com/lbrihoum/FluffyFeelings');
-    window.open('https://github.com/lbrihoum/Doggos');
-});
